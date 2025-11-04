aff31896d106e805cb3a84d4f1fbe5852b3cb99f
Emanuel Forlin <emaa.forlin@gmail.com>
2025-11-04 08:40:11 -0300

    feat: add document model with db migrations

---
+++ cmd/migrator/migrate.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/emaforlin/ce-document-service/config"
+	"github.com/emaforlin/ce-document-service/models"
+	"github.com/emaforlin/ce-document-service/repository"
+)
+
+type DatabaseMigrator struct {
+	repo *repository.PostgresDocumentRepositoryImpl
+}
+
+func NewMigrator(cfg config.DatabaseConfig) *DatabaseMigrator {
+	panic("unimplemented")
+}
+
+// RunMigrations executes all database migrations
+func (m *DatabaseMigrator) RunMigrations() error {
+	log.Println("Starting database migrations...")
+
+	// Add all models that need to be migrated here
+	models := []interface{}{
+		&models.Document{},
+	}
+
+	if err := m.repo.GetDB().AutoMigrate(models...); err != nil {
+		log.Printf("Migration failed: %v", err)
+		return err
+	}
+
+	log.Println("Database migrations completed successfully!")
+	return nil
+}
+
+func main() {
+	cfg := config.GetConfig()
+	migrator := NewMigrator(cfg.GetDatabaseConf())
+
+	if err := migrator.RunMigrations(); err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/emaforlin/ce-document-service
+
+go 1.24.5
+
+require (
+	github.com/jackc/pgtype v1.14.4
+	gorm.io/gorm v1.31.1
+)
+
+require (
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
