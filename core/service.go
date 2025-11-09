a08291e1bf40c69825b1ccdb2ef6d0398f81652b
Emanuel Forlin <emaa.forlin@gmail.com>
2025-11-09 18:48:47 -0300

    feat: add DocumentService and DTOs for document creation and retrieval

---
+++ core/service.go
@@ -0,0 +1,47 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/emaforlin/ce-document-service/dto"
+	"github.com/emaforlin/ce-document-service/models"
+	"github.com/emaforlin/ce-document-service/repository"
+)
+
+type DocumentService struct {
+	repo repository.DocumentRepository
+}
+
+func (s *DocumentService) CreateNewDocument(data dto.CreateDocumentDTO) (string, error) {
+	docID, err := s.repo.CreateDocument(models.Document{
+		Title:   data.Title,
+		OwnerID: data.OwnerID,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to create a new document: %w", err)
+	}
+	return docID, nil
+}
+
+func (s *DocumentService) GetUserDocuments(ownerID string) ([]models.Document, error) {
+	documents, err := s.repo.GetAllDocuments(ownerID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch user documents: %w", err)
+	}
+	return documents, nil
+}
+
+func (s *DocumentService) GetOneDocument(data dto.GetOneDocumentDTO) *models.Document {
+	document := s.repo.GetOneDocument(data.OwnerID, data.DocumentID)
+	return document
+}
+
+func NewDocumentService(documentsRepository repository.DocumentRepository) (*DocumentService, error) {
+	if documentsRepository == nil {
+		return nil, fmt.Errorf("error creating the documents service: documentsRepository cannot be nil")
+	}
+
+	return &DocumentService{
+		repo: documentsRepository,
+	}, nil
+}
